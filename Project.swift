647d984066d40927a9a8badc098d02a25633a33f
simyo <hj.sim@muzlive.com>
2023-08-09 16:08:34 +0900

    fix: tuist 초기세팅

---
+++ Project.swift
@@ -0,0 +1,33 @@
+// Project.swift
+
+
+import ProjectDescription
+
+let project = Project(
+    name: "SwitchTuistFinalTest",
+    organizationName: "simyo",
+    settings: baseSettings(),
+    targets: [
+        Target(
+            name: "SwitchTuistFinalTest",
+            platform: .iOS,
+            product: .app,
+            bundleId: "switchTuistFinalTest.simyo.com",
+            deploymentTarget: .iOS(targetVersion: "13.0", devices: [.iphone, .ipad]),
+            infoPlist: "SwitchTuistFinalTest/Info.plist",
+            sources: ["SwitchTuistFinalTest/**"],
+            resources: nil,
+            dependencies: []
+        )
+    ]
+)
+
+private func baseSettings() -> Settings {
+    var settings = SettingsDictionary()
+    return Settings.settings(base: settings,
+                             configurations: [
+                                .debug(name: "Debug", xcconfig: .relativeToRoot("SwitchTuistFinalTest.xcconfig"))
+                             ],
+                             defaultSettings: .recommended(excluding: ["ASSETCATALOG_COMPILER_APPICON_NAME"])
+    )
+}
