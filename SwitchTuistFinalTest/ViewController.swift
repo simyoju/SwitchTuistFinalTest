655cbc8d906f1fbd2691522be2ba41fcae661ebb
simyo <hj.sim@muzlive.com>
2023-08-09 14:19:26 +0900

    Initial Commit

---
+++ SwitchTuistFinalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwitchTuistFinalTest
+//
+//  Created by Muzlive_Player on 2023/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
